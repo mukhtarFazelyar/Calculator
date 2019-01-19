221b21de10bbbedccaf3476d5baf4bed0526b64e
mukhtarFazelyar <46763220+mukhtarFazelyar@users.noreply.github.com>
2019-01-19 13:06:03 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,6 @@ public class Calculator {
     public static double subtract(double num1,double num2){
         return  num1-num2;
     }
+    public static double multiply(double num1,double num2){
+        return nume1*num2;
 }
